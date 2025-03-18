869bbc3f7ce3bfedfc65a17f733b8d8687490e60
amplastictech <admin@americanplastictech.com>
2025-03-18 12:14:48 -0700

    Update review_checker.py

--- review_checker.py
+++ review_checker.py
@@ -22,11 +22,11 @@ SMTP_PORT = 587
 def send_email(subject, body):
     msg = MIMEText(body)
     msg['Subject'] = subject
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = RECIPIENT
+    msg['From'] = EMAIL_SENDER  # Changed from EMAIL_ADDRESS
+    msg['To'] = EMAIL_RECEIVER  # Changed from RECIPIENT
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
         server.starttls()
-        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.login(EMAIL_SENDER, EMAIL_PASSWORD)  # Changed from EMAIL_ADDRESS
         server.send_message(msg)
 
 def check_reviews():
